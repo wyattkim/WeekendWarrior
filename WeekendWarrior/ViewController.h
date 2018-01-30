8cb9287dc933568630894b500a6dbdb0e514c39c
Wyatt Kim <wyattkim@Wyatts-MacBook-Pro.local>
2018-01-29 16:18:43 -0800

    adding launch screen

---
+++ WeekendWarrior/ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  Warrior
+//
+//  Created by Alana Kamahele on 1/24/18.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *customTextField;
+@property (weak, nonatomic) IBOutlet UILabel *helloLabel;
+@property (weak, nonatomic) IBOutlet UIButton *clickyButton;
+@property (weak, nonatomic) IBOutlet UILabel *motto;
+@property (weak, nonatomic) IBOutlet UIImageView *background;
+@property (weak, nonatomic) IBOutlet UISlider *hourDistancePicker;
+@property (weak, nonatomic) IBOutlet UILabel *hourDistanceLabel;
+@property (weak, nonatomic) IBOutlet UIDatePicker *myDatePicker;
+
+@end
+
---
+++ WeekendWarrior/ViewController.m
@@ -0,0 +1,44 @@
+//
+//  ViewController.m
+//  Warrior
+//
+//  Created by Alana Kamahele on 1/24/18.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.myDatePicker.datePickerMode = UIDatePickerModeDateAndTime;
+}
+
+- (IBAction)handleSearchClick:(id)sender {
+    self.helloLabel.text = @"Weekend Warrior";
+}
+- (IBAction)distanceSliderChanged:(id)sender {
+    // Set the label text to the value of the slider as it changes
+    self.hourDistanceLabel.text = [NSString stringWithFormat:@"%d hr",
+        (int) roundf(self.hourDistancePicker.value * 9) + 1];
+    
+    
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.customTextField resignFirstResponder];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
