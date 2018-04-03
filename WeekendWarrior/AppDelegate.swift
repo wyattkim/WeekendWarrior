d47b310dd632852c99aa27d1e8ba734e385a6410
Wyatt Kim <wyattkim@Wyatts-MacBook-Pro.local>
2018-04-02 23:58:28 -0700

    adding user sign in

--- WeekendWarrior/AppDelegate.swift
+++ WeekendWarrior/AppDelegate.swift
@@ -11,6 +11,7 @@ import GoogleMaps
 import GooglePlaces
 import Firebase
 import Flurry_iOS_SDK
+import Lock
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +21,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, open url: URL,
                      sourceApplication: String?, annotation: Any) -> Bool {
         return true;
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        return Lock.resumeAuth(url, options: options)
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
--- WeekendWarrior/TrailDisplayController.swift
+++ WeekendWarrior/TrailDisplayController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import MapKit
+import Lock
+import Auth0
 
 class TrailDisplayController: UIViewController {
     var trail = Trail(json: ["": "" as AnyObject])
@@ -19,6 +21,44 @@ class TrailDisplayController: UIViewController {
     @IBOutlet weak var statusButton: UIButton!
     @IBOutlet weak var weatherSummary: UILabel!
     @IBOutlet weak var directionsButton: UIButton!
+    
+    @IBAction func showLoginController(_ sender: UIButton) {
+        Lock
+            .classic()
+            .withOptions {
+                $0.closable = true
+                $0.oidcConformant = true
+                $0.passwordManager.enabled = false
+            }
+            .withStyle {
+                $0.title = "Weekend Warrior"
+                $0.logo = LazyImage(name: "AppIcon")
+            }
+            .onAuth {
+                print("Obtained credentials \($0)")
+                //self.accessToken = $0.accessToken!
+                Auth0
+                    .authentication()
+                    .userInfo(withAccessToken: $0.accessToken!)
+                    .start { result in
+                        switch result {
+                        case .success(let profile):
+                            // You've got a UserProfile object
+                            print(profile.email)
+                        case .failure(let error):
+                            // You've got an error
+                            print("Failed with \(error)")
+                        }
+                }
+            }
+            .onError {
+                print("Failed with \($0)")
+            }
+            .onCancel {
+                print("User cancelled")
+            }
+            .present(from: self)
+    }
     
     struct Defaults {
         
